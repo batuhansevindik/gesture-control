6e7569b155bd0688e461d6921cd482f9476cbdaf
Batuhan Sevindik <batuhansevindik12@gmail.com>
2026-07-06 18:04:17 +0200

    Remove experimental and personal automation scripts; keep core application

--- start_cross_lock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "$(date) start_cross_lock calisti" >> /home/batuhans/gesture-control/lock_watcher.log
-sleep 3
-cd /home/batuhans/gesture-control
-source /home/batuhans/gesture-control/venv/bin/activate
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
-python /home/batuhans/gesture-control/cross_lock.py
